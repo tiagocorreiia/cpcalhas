fee19871656d0ca5bad7ff3b9677a0a20888cf7b
Tiago Correia <tiagocorreiia@gmail.com>
2021-03-18 13:57:40 -0300

    Initial commit

--- php/sendmail.php
+++ php/sendmail.php
@@ -21,10 +21,10 @@ try {
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->CharSet = 'UTF-8';
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.hotmail.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'termosteel.com.br';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'Cpcalhas@hotmail.com';                     // SMTP username
-    $mail->Password   = 'termosteel34638581';                               // SMTP password
+    $mail->Username   = 'info@termosteel.com.br';                     // SMTP username
+    $mail->Password   = 'Fyb@071285b';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
